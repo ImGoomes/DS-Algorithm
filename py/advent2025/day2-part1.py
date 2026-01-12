d1f5383cafcb06f2990771c3e4bb93be6e50c6a2
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2026-01-12 12:40:53 +0000

    refactor: minor changes

--- py/advent2025/day2-part1.py
+++ py/advent2025/day2-part1.py
@@ -5,7 +5,7 @@
 # This function will recive the raw input range of number in
 # string format and convert it into a matrix of integer
 # return a matrix of integer
-def format_range(raw_range):
+def format_range(raw_range: str) -> list[list[int]]:
     range_matrix = []
     split_range = raw_range.split(",")
 
@@ -13,7 +13,7 @@ def format_range(raw_range):
         range_array = []
         range_numbers = item.split("-")
 
-        # Verify if is a valid range (only two numbers)
+        # Verify if is a valid range (two or higher)
         if len(range_numbers) <= 2:
             start = int(range_numbers[0])
             end = int(range_numbers[1])
@@ -34,7 +34,7 @@ def format_range(raw_range):
 # find_invalid_ids(range_matrix: [[int]]): [int]
 # This function is find the invalid IDs existing int the range
 # return a integer array of all invalid IDs
-def find_invalid_ids(range_matrix):
+def find_invalid_ids(range_matrix: list[list[int]]) -> list[int]:
     invalid_ids_range = []
 
     for range_array in range_matrix:
@@ -45,7 +45,7 @@ def find_invalid_ids(range_matrix):
             # Verify if the number is divisible by 2
             # Req: The number must be repeted twice only
             if len(item_array) % 2 == 0:
-                slice_index = int(len(item_array) / 2)
+                slice_index = len(item_array) // 2
 
                 first_slice = int("".join(map(str, item_array[:slice_index])))
                 second_slice = int("".join(map(str, item_array[slice_index:])))
@@ -59,18 +59,12 @@ def find_invalid_ids(range_matrix):
 # sum_invalid_ids(invalid_ids: [int]): int
 # This function is responsible to sum all invalid_ids
 # return the total amount
-def sum_invalid_ids(invalid_ids):
-    total = 0
-    for item in invalid_ids:
-        total += item
-
-    return total
+def sum_invalid_ids(invalid_ids: list[int]) -> int:
+    return sum(invalid_ids)
 
 
 def main() -> None:
     raw_range = "269194394-269335492,62371645-62509655,958929250-958994165,1336-3155,723925-849457,4416182-4470506,1775759815-1775887457,44422705-44477011,7612653647-7612728309,235784-396818,751-1236,20-36,4-14,9971242-10046246,8796089-8943190,34266-99164,2931385381-2931511480,277-640,894249-1083306,648255-713763,19167863-19202443,62-92,534463-598755,93-196,2276873-2559254,123712-212673,31261442-31408224,421375-503954,8383763979-8383947043,17194-32288,941928989-941964298,3416-9716"
-    invalid_ids = []
-    ranges = []
 
     ranges = format_range(raw_range)
     invalid_ids = find_invalid_ids(ranges)
