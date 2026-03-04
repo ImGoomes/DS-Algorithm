3691932d6a09bc1843b4e4d5eb36a274757bccc8
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2026-03-04 09:02:15 +0000

    dynamic array

---
+++ c/arrays/dynamic_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* This program shoud read integers from a user input
+ * and dynamically add it to an a array and print the
+ * array with the new item. */
+
+int main() {
+}
+
+/* Function to validate array capacity*/
