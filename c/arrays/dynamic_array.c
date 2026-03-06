4fd3414b2cd49cb132738e17ff6520604b5eac19
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2026-03-06 08:29:33 +0000

    dynamic array

--- c/arrays/dynamic_array.c
+++ c/arrays/dynamic_array.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* This program shoud read integers from a user input
- * and dynamically add it to an a array and print the
- * array with the new item. */
+typedef struct {
+  int *data;
+  int size;
+  int capacity;
+} Array;
+
 
 int main() {
+
 }
 
-/* Function to validate array capacity*/
+int next_power_of_2(int n) {
+  int cap = 16;
+
+  while (cap < n){
+    cap = cap * 2;
+  }
+  return cap;
+}
+
+Array *array_new(int inital_capacity){
+  Array *arr = (Array *)malloc(sizeof(Array));
+  arr->capacity = next_power_of_2(inital_capacity);
+  arr->data = (int *)malloc(arr->capacity * sizeof(int));
+  arr->size = 0;
+  return arr;
+}
