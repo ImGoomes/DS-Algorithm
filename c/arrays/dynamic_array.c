1b1974f8401c8cf9e2f728bc13b6641e0d1461a1
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2026-03-10 09:27:43 +0000

    resize function

--- c/arrays/dynamic_array.c
+++ c/arrays/dynamic_array.c
@@ -25,6 +25,11 @@ Vector *vector_new(int inital_capacity){
   return v;
 }
 
+void resize(Vector *v){
+  v->capacity = v->capacity * 2;
+  v->data = (int *)realloc(v->data, v->capacity * sizeof(int));
+}
+
 int vector_size(Vector *v){
   return v->size;
 }
@@ -44,8 +49,7 @@ int vector_at(Vector *v, int index){
 
 void vector_push(Vector *v, int value){
   if(v->size == v->capacity){
-    v->capacity = v->capacity * 2;
-    v->data = (int *)realloc(v->data, v->capacity * sizeof(int));
+    resize(v);
   }
 
   *(v->data + v-> size) = value;
@@ -56,8 +60,7 @@ void insert_at(Vector *v, int value, int index){
   int i = v->size;
 
   if (v->capacity == v->size){
-    v->capacity = v->capacity * 2;
-    v->data = (int *)realloc(v->data, v->capacity * sizeof(int));
+    resize(v);
   }
  
   while (i > index) {
@@ -72,7 +75,7 @@ void delete_at(Vector *v, int index){
   if(index >= v->size){
     printf("Array out of bounds\n");
     exit(1);
-  }
+}
 
   while(index < v->size - 1){
     *(v->data + index) = *(v->data + (index + 1));
