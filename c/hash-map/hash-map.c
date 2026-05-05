f63e3191e45646712564b7acfe682b874a2cf714
Gabriel Gomes <gabrielsousagomes@hotmail.com>
2026-05-05 10:14:05 +0100

    hash map: create struct slot for linear probing

--- c/hash-map/hash-map.c
+++ c/hash-map/hash-map.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
+// Implement hash map using linear probing
 
-# Implement hash map using linear probing
-# hash(k, m) - k = key / m = size
-# add(key, value)
-# exists(key)
-# get(key)
-# remove(key)
+enum State {
+  EMPTY,
+  BUSY,
+  REMOVED
+};
+
+typedef struct {
+  int key;
+  int value;
+  enum State state;
+} Slot;
+
+int main() {
+
+}
+
+// hash(k, m) - k = key / m = size
+// add(key, value)
+// exists(key)
+// get(key)
+// remove(key)
 
